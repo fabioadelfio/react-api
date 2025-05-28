eabf3324f6328e5fe9933865e6eb5a2b719e9537
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-28 14:45:30 +0200

    Milestone 1

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Main() {
+
+  const apiUrl = "https://lanciweb.github.io/demo/api/actors/";
+
+  const [actors, setActors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then(response => {
+        console.log(`Dati ricevuti:`, response.data);
+        setActors(response.data);
+      })
+
+      .catch(error => {
+        console.log(`Errore nel recupero dati:`, error)
+      })
+
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main>
-      <h1>App React</h1>
+      
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./assets/css/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
